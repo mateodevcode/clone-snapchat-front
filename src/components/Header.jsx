0f862f6e646d1f2cbf7feae0b1bfab3694cc70fb
Proservices Ingenieria <reydavidquiroz@gmail.com>
2023-12-22 23:51:14 -0500

    Header terminado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import "../Header.css";
+import "../main.css";
 import { FaSnapchat } from "react-icons/fa6";
 import { VscVersions } from "react-icons/vsc";
 import { VscPlay } from "react-icons/vsc";
@@ -12,67 +12,66 @@ import { CgSearch } from "react-icons/cg";
 
 const Header = () => {
   return (
-    <header className="flex w-full items-center header  ">
+    <nav className="w-full h-16 pt-2 flex flex-row items-center header justify-between fixed">
+      {" "}
       {/* Cuadro principal del header */}
-      <div>
-        <div className="flex  items-center text-center">
-          <div className=" flex items-center text-center h-20px w-20px ">
-            <div className="flex h-16 w-16 p-4 pl-6   items-center justify-center ">
-              <FaSnapchat className="text-2xl" />
-            </div>
-          </div>
-          <div className="flex w-full items-center text-center relative ml-2 ">
-            <input
-              type="text"
-              placeholder="Buscar"
-              className="rounded-full h-10 w-48 text-center text-sm font-semibold"
-            />
-            <div className="absolute ml-2">
-              <CgSearch className="text-2xl" />
-            </div>
+      {/* Primer Div */}
+      <div className="flex flex-row justify-center items-center ml-1 mr-20 ">
+        <div className="flex items-center justify-center mx-6 cursor-pointer">
+          <FaSnapchat className="text-2.2xl" />
+        </div>
+        <div className="flex w-full items-center">
+          <input
+            type="text"
+            placeholder="Buscar"
+            className="pl-10 w-44 rounded-full py-2"
+          />
+          <div className="absolute ml-2">
+            <CgSearch className="text-2xl" />
           </div>
         </div>
       </div>
-      <div className="flex items-center ml-52 mr-20">
-        <nav className=" ">
-          <ul className="flex items-center w-full ">
-            <li>
-              <div className="botones">
-                <VscVersions className="text-2xl" />
-                <span className="flex-end">Historias</span>
-              </div>
-            </li>
-            <div className=" botones">
-              <VscPlay className="text-2xl" />
-              <span>Spolight</span>
-            </div>
-            <div className=" botones">
-              <BsChatRight className="text-2xl" />
-              <span>Chat</span>
-            </div>
-            <div className=" botones">
-              <WiStars className="text-2xl" />
-              <span>Lentes</span>
-            </div>
-            <div className=" botones">
-              <TbMapPin className="text-2xl" />
-              <span>Mapa</span>
-            </div>
-          </ul>
-        </nav>
+      {/* Segundo Div */}
+      <div className="flex flex-row justify-center items-center">
+        <ul className="flex flex-row justify-center items-center">
+          <li className="flex flex-col justify-center items-center mx-1 cursor-pointer color-bg w-16 h-16 rounded-md ">
+            <VscVersions className="text-2xl mb-1" />
+            <p className="text-enlace font-semibold">Historias</p>
+          </li>
+          <li className="flex flex-col justify-center items-center mx-1 cursor-pointer color-bg w-16 h-16 rounded-md">
+            <VscPlay className="text-2xl mb-1" />
+            <p className="text-enlace font-semibold ">Spolight</p>
+          </li>
+          <li className="flex flex-col justify-center items-center mx-1 cursor-pointer color-bg w-16 h-16 rounded-md">
+            <span className="bg-sky-500 text-white nube-chat rounded-full py absolute top-2 cursor-pointer">
+              NEW
+            </span>
+            <BsChatRight className="text-2xl mb-1" />
+            <p className="text-enlace font-semibold ">Chat</p>
+          </li>
+          <li className="flex flex-col justify-center items-center mx-1 cursor-pointer color-bg w-16 h-16 rounded-md">
+            <WiStars className="text-2xl mb-1 border-2 border-black rounded-full" />
+            <p className="text-enlace font-semibold ">Lentes</p>
+          </li>
+          <li className="flex flex-col justify-center items-center mx-1 cursor-pointer color-bg w-16 h-16 rounded-md">
+            <TbMapPin className="text-2xl mb-1" />
+            <p className="text-enlace font-semibold ">Mapa</p>
+          </li>
+        </ul>
       </div>
-      <div className="flex items-center ml-10">
-        <nav className="">
-          <div className="bg-white rounded-full w-10 h-10 flex items-center justify-center">
-            <CgMenuGridO className="text-2xl " />
-          </div>
-        </nav>
-        <div className="boton-negro text-center ml-15 mr-3 ">
-          Anuncios de Snapchat
+      {/* Tercer Div */}
+      <div className="flex flex-row justify-center items-center mr-4">
+        <div className="bg-white rounded-full p-2 mx-4 cursor-pointer hover:bg-gray-400">
+          <CgMenuGridO className="text-2xl " />
         </div>
-        <div className="boton-negro w-1/4 text-center ">Descargar</div>
+        <div className="text-white bg-gray-900 rounded-full px-4 py-1.5 mx-1.5 cursor-pointer">
+          <p className="text-sm font-semibold">Anuncios de Snapchat</p>
+        </div>
+        <div className="text-white bg-gray-900 rounded-full px-4 py-1.5 mx-1.5 cursor-pointer">
+          <p className="text-sm font-semibold">Descargar</p>
+        </div>
       </div>
-    </header>
+    </nav>
   );
 };
 
